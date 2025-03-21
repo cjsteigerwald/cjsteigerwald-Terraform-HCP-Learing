e2f51f90fae7fbfb37e32fa6c4bf1c402b97810d
Christopher Steigerwald <cjsteigerwald@gmail.com>
2025-03-21 13:34:14 -0700

    fixing secrets

--- providers.tf
+++ providers.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   features {}
   subscription_id = "d9d4d093-f4dc-4689-a1a2-7ee1dfd134cf"
   tenant_id       = "bad8ac1e-3ca0-400e-9320-2fd81fde74d4"
-  client_id       = "84a2dab3-72e4-4f6a-a6cb-a2372247134e"
-  client_secret   = ""
+  client_id       = "service-principal-key-client-id"
+  client_secret   = "service-principal-key-client-secret"
 }
