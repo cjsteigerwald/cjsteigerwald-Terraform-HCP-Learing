57b908484e4f2ff976407fc78821a976f04c1bf8
Christopher Steigerwald <cjsteigerwald@gmail.com>
2025-03-21 13:58:37 -0700

    test

--- providers.tf
+++ providers.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   features {}
   subscription_id = "d9d4d093-f4dc-4689-a1a2-7ee1dfd134cf"
   tenant_id       = "bad8ac1e-3ca0-400e-9320-2fd81fde74d4"
-  client_id       = "service-principal-key-client-id"
-  client_secret   = "service-principal-key-client-secret"
+  client_id       = "var.service-principal-key-client-id"
+  client_secret   = "var.service-principal-key-client-secret"
 }
