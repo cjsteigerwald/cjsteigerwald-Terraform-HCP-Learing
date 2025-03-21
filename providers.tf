62f342a4d21a07d00833e3aadfca22d00738099a
Christopher Steigerwald <cjsteigerwald@gmail.com>
2025-03-21 14:07:46 -0700

    removing provider

--- providers.tf
+++ providers.tf
@@ -9,8 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "d9d4d093-f4dc-4689-a1a2-7ee1dfd134cf"
-  tenant_id       = "bad8ac1e-3ca0-400e-9320-2fd81fde74d4"
-  client_id       = "var.service-principal-key-client-id"
-  client_secret   = "var.service-principal-key-client-secret"
 }
